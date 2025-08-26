c8175abef4d3c4813291143ed9d0478c77f20674
るび7777 <rubibobu@gmail.com>
2025-08-26 12:38:37 +0900

    Fix comments in SQL files

--- sql_base_answer1.sql.sql
+++ sql_base_answer1.sql.sql
@@ -1,48 +1,48 @@
---1
+-- 1
 SELECT uniform_num, name, club
 FROM players;
 
---2
+-- 2
 SELECT *
 FROM countries
 WHERE group_name = 'C';
 
---3
+-- 3
 SELECT *
 FROM countries
 WHERE group_name <> 'C';   
 
---4
+-- 4
 SELECT *
 FROM players
 WHERE birth <= '1985-08-25';   
 
---5
+-- 5
 SELECT *
 FROM players
 WHERE height < 170;   
 
---6
+-- 6
 SELECT *
 FROM countries
 WHERE ranking BETWEEN 36 AND 56;
 
---7
+-- 7
 SELECT *
 FROM players
 WHERE position IN ('GK', 'DF', 'MF');
 
---8
+-- 8
 SELECT *
 FROM goals
 WHERE player_id IS NULL;
 
---9
+-- 9
 SELECT *
 FROM goals
 WHERE player_id IS NOT NULL;
 
---10
+-- 10
 SELECT *
 FROM players
 WHERE name LIKE '%ニョ';
